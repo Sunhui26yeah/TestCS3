63f321431f618482e733419fbcc2c8f40c1c8974
hsun26 <hsun26@ncsu.edu>
2024-09-08 19:51:27 -0400

    newV

--- src/main/java/org/example/oldV.java
+++ src/main/java/org/example/oldV.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class oldV {
     public static double snippet(int x, int y) {
-        if (x*x*x > 0){
+        if (x*x*x < 0){//change
             if(x>0 && y==10)
                 return 1000;
         } else {
